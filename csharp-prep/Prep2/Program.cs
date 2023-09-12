2bd4cab2984a1da14c5968cb21e6ec397e9cf49d
Dmytro <dima4994@gmail.com>
2023-09-12 19:35:07 +0300

    W1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your scale: ");
+        int scale = int.Parse(Console.ReadLine());
+        string letterGrade = "";
+        switch(scale) 
+        {
+            case >= 90:
+                letterGrade = "A";
+                break;
+            case >= 80:
+                letterGrade = "B";
+                break;
+            case >= 70:
+                letterGrade = "C";
+                break;
+            case >= 60:
+                letterGrade = "D";
+                break;
+            default:
+                letterGrade = "F";
+                break;
+        }
+
+        if (scale >= 70) 
+        {
+            Console.WriteLine($"Congratulation you have passed the coursee! Your grade is {letterGrade}!");
+        }
+        else 
+        {
+            Console.WriteLine($"Unforthunatly you have not passed the coursee! Your grade is {letterGrade}!");
+        }
     }
 }
