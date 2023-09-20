c8882dcbb9414c001f3f3dd6c43240e32d1cd7da
Dmytro <dima4994@gmail.com>
2023-09-20 16:38:37 +0300

    W2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guessAmount = 0;
+        int number = new System.Random().Next(0, 101);
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        
+        do {
+            guessAmount += 1;
+            if (number < guess) {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                continue;
+            } else if (number > guess) {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+            }
+        } while (number != guess);
+
+        Console.WriteLine($"You guessed it! You have {guessAmount} guesses!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number;
+        List<int> numbers = new List<int>();
+
+        do {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        } while (number != 0);
+
+        numbers.Remove(0);
+        numbers.Sort();
+
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int largest = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine("The sorted list is:");
+        numbers.ForEach(Console.WriteLine);
     }
 }
